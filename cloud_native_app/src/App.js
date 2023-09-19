10ba836fad7e46aa6694ae4393cf56b217b07eb7
Basti <sebastian.hufeld@docc.techstarter.de>
2023-09-19 13:26:17 +0200

    dritter Commit

--- cloud_native_app/src/App.js
+++ cloud_native_app/src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Das ist ein Test!
+          Das ist ein wunderbarer Test!
         </p>
         <a
           className="App-link"
